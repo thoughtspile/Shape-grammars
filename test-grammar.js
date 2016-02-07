f98c60048732678ba30aa9216bf76faf94cec951
thoughtspile <v.klepov@gmail.com>
2016-02-07 21:03:16 +0300

    no features added

--- test-grammar.js
+++ test-grammar.js
@@ -9,19 +9,21 @@
     gen.init(gen.init().resize([300, 300]));
     var rectPrev = gen.init();
 
-    [0,1,2,3,4,5].forEach(function() {
+    [0,1,2].forEach(function() {
         var rect = gen.addState(function(parent) {
-            this.scope.size = parent.scope.size.map(x => x / 2);
             this.color = randClr(null, null, null, 1);
-            this.trace = (parent.trace || []).concat([parent.id]);
         });
         gen.rule(rectPrev, function(parent) {
-            var offset = parent.scope.size.map(x => x / 2);
+            var size = parent.scope.size.slice();
+            var ratio = Math.random();
             return [
-                rect,
-                rect.mv([0, offset[1]]),
-                rect.mv([offset[0], 0]),
-                rect.mv(offset)
+                rect.resize([size[0] * ratio, size[1] * ratio]),
+                rect.mv([0, ratio * size[1]])
+                    .resize([size[0] * ratio, size[1] * (1 - ratio)]),
+                rect.mv([ratio * size[0], 0])
+                    .resize([size[0] * (1 - ratio), size[1] * ratio]),
+                rect.mv([ratio * size[0], ratio * size[1]])
+                    .resize([size[0] * (1 - ratio), size[1] * (1 - ratio)])
             ]
         });
         rectPrev = rect;
